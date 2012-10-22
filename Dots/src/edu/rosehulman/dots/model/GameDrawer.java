74fad4d6c6208f0f257bc58ac2f5a3533f7300f5
Andrew Siegle <asiegle@gmail.com>
2012-10-21 22:42:48 -0400

    added gameDrawer and new game xml

--- Dots/src/edu/rosehulman/dots/model/GameDrawer.java
+++ Dots/src/edu/rosehulman/dots/model/GameDrawer.java
@@ -1,30 +1,90 @@
 package edu.rosehulman.dots.model;
 
-public class GameDrawer {
-	int width;
-	int height;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.View;
+import edu.rosehulman.dots.R;
+
+public class GameDrawer extends View {
+	int width = 5;
+	int height = 5;
 	int boxHeight;
-	
-	public GameDrawer(int w, int h){
-		width = w;
-		height = h;
+	Paint paint;
+	private final int PADDING = 5;
+	private final int DOT_SIZE = 3;
+	private final int BORDER_STROKE = 3;
+
+	public GameDrawer(Context context) {
+		super(context);
+		paint = new Paint();
 	}
-	
-	public void drawBoard(){
-		drawDots();
-		drawLines();
-		drawSquares();
+
+	private void drawLines(Canvas canvas) {
 	}
-	
-	private void drawLines(){
-		
+
+	private void drawBorder(Canvas canvas) {
+		paint.setStrokeWidth(BORDER_STROKE);
+		paint.setStyle(Paint.Style.STROKE);
+		paint.setColor(getResources().getColor(R.color.black));
+		canvas.drawRect(PADDING, PADDING, getWidth() - PADDING, getHeight()
+				- PADDING, paint);
 	}
-	private void drawSquares(){
-		
+
+	private void drawSquares(Canvas canvas) {
+
 	}
-	private void drawDots(){
-		
+
+	private void drawDots(Canvas canvas) {
+		int extraPaddingFromBorder = PADDING + BORDER_STROKE;
+		int xDistanceBetweenDots = (getWidth() - extraPaddingFromBorder * 2)
+				/ width + 1;
+		int yDistanceBetweenDots = (getHeight() - extraPaddingFromBorder * 2)
+				/ height + 1;
+
+		int xPosition;
+		int yPosition;
+		int xPadding = xDistanceBetweenDots / 2 + extraPaddingFromBorder;
+		int yPadding = yDistanceBetweenDots / 2 + extraPaddingFromBorder;
+		for (int i = 0; i < width; i++) {
+			xPosition = xPadding + xDistanceBetweenDots * i;
+			for (int z = 0; z < height; z++) {
+				yPosition = yPadding + yDistanceBetweenDots * z;
+				canvas.drawCircle(xPosition, yPosition, DOT_SIZE, paint);
+			}
+		}
 	}
-	
+
+	@Override
+	public void onDraw(Canvas canvas) {
+		drawLines(canvas);
+		drawSquares(canvas);
+		drawDots(canvas);
+		drawBorder(canvas);
+	}
+
+	public int getViewWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getViewHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public int getBoxHeight() {
+		return boxHeight;
+	}
+
+	public void setBoxHeight(int boxHeight) {
+		this.boxHeight = boxHeight;
+	}
 
 }
